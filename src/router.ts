bddae876b6aa93fdc8f0c14f63fa2be2af5e6b36
VACson <memedealer1337@gmail.com>
2023-11-07 20:01:27 +0200

    feat(Auth): Change router imports

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-import Auth from "./views/Auth.vue"
-import Dashboard from "./views/Dashboard.vue"
-
 const routes = [
-	{ path: "/", component: () => Dashboard, name: "Dashboard" },
-	{ path: "/login", component: () => Auth, name: "Login" }
+	{ path: "/", component: () => import("./views/Dashboard.vue"), name: "Dashboard" },
+	{ path: "/login", component: () => import("./views/Auth.vue"), name: "Login" }
 ]
 
 const router = createRouter({
