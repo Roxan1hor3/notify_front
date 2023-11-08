4c6de9cca6381c26eba7c2a9e4dc02c5d85c7699
VACson <memedealer1337@gmail.com>
2023-11-08 23:41:26 +0200

    feat(Auth): Change login request

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,30 +3,39 @@ import { API } from "../utils/http"
 import router from "../router"
 
 type User = {
-	username: string
-	password: string
+  username: string
+  password: string
 }
 
 export const loginUser = async ({ username, password }: User) => {
-	const token = username + ":" + password
-	try {
-		const { data } = await API.post(
-			"auth/login",
-			{},
-			{
-				headers: {
-					Authorization: `Basic ${btoa(token)}`
-				}
-			}
-		)
-		router.push({ name: "Dashboard" })
+  const data = new FormData()
+  data.append("username", username)
+  data.append("password", password)
+  try {
+    const res = await API.post("auth/login", data)
+    router.push({ name: "Dashboard" })
 
-		return data
-	} catch (e: any) {
-		message.error(e?.response?.data || "Error happend on login request")
-	}
+    API.interceptors.request.use(
+      (config) => {
+        if (res.data.access_token) {
+          config.headers["Authorization"] = "Bearer " + res.data.access_token
+        }
+        config.headers["Content-Type"] = "application/json"
+        return config
+      },
+      (error) => {
+        Promise.reject(error)
+      }
+    )
+
+    return res.data
+  } catch (e: any) {
+    console.log(e)
+
+    message.error(e?.response?.data || "Error happend on login request")
+  }
 }
 
 export const getUser = () => {
-	return API.get("auth/profile")
+  return API.get("notify/profile")
 }
